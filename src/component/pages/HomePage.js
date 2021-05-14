772921567c50a040f26aa97408b5ac6ed04f71ea
Fei Ye <feiye@Feis-MacBook-Pro.local>
2021-05-14 22:08:17 +1000

    added routes

---
+++ src/component/pages/HomePage.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+    <div>
+        <h1>Welcome to homepage</h1> 
+    </div>
+    );
+  }
+}
+
+export default HomePage;
---
+++ src/component/pages/QuizCreatePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class QuizCreatePage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( <div><h1>Quiz create</h1></div> );
+    }
+}
+ 
+export default QuizCreatePage;
