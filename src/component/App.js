0fba143d788ec4f6755957304d539e8553b01a98
Fei Ye <feiye@Feis-MacBook-Pro.local>
2021-05-15 16:10:57 +1000

    merge conflict in App.js

--- src/component/App.js
+++ src/component/App.js
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
-import QuizItem from "./QuizItem"
-
-function App() {
-  return (
-    <div className="App">
-        <QuizItem />
-    </div>
-=======
 import React from "react";
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import GamePlayPage from "./pages/GamePlayPage";
 import GameStartPage from "./pages/GameStartPage";
@@ -16,8 +7,8 @@ import HostSignInPage from "./pages/HostSignInPage";
 import PlayerSignInPage from "./pages/PlayerSignInPage";
 import QuizCreatePage from "./pages/QuizCreatePage";
 import QuizIndexPage from "./pages/QuizIndexPage";
+import QuizItem from "./QuizItem";
 import Nav from "./Nav";
-
 
 function App() {
   return (
@@ -25,19 +16,18 @@ function App() {
       <div>
         <Nav />
         <Switch>
-          <Route exact path="/" component={HomePage}/>;
-          <Route exact path="/gameplay" component={GamePlayPage}/>;
-          <Route exact path="/gamestart" component={GameStartPage}/>;
-          <Route exact path="/host-sign-in" component={HostSignInPage}/>;
-          <Route exact path="/player-sign-in" component={PlayerSignInPage}/>;
-          <Route exact path="/quiz-create" component={QuizCreatePage}/>;
-          <Route exact path="/quiz-index" component={QuizIndexPage}/>;
+          <Route exact path="/" component={HomePage} />;
+          <Route exact path="/gameplay" component={GamePlayPage} />;
+          <Route exact path="/gamestart" component={GameStartPage} />;
+          <Route exact path="/host-sign-in" component={HostSignInPage} />;
+          <Route exact path="/player-sign-in" component={PlayerSignInPage} />;
+          <Route exact path="/quiz-create" component={QuizCreatePage} />;
+          <Route exact path="/quiz-index" component={QuizIndexPage} />;
         </Switch>
+        <QuizItem />
       </div>
     </Router>
->>>>>>> 772921567c50a040f26aa97408b5ac6ed04f71ea
   );
 }
 
 export default App;
-
