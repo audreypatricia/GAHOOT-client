7f8347d3af08f7bc5897acc6e3f303f4d4ff9836
Fei Ye <feiye@Feis-MacBook-Pro.local>
2021-05-15 16:19:34 +1000

    fix syntax error in PlayButton.js

--- src/component/PlayButton.js
+++ src/component/PlayButton.js
@@ -1,24 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class PlayButton extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      quiz: []
-    }
-    console.log(props)
+      quiz: [],
+    };
+    console.log(props);
   }
 
   _handleQuizSelection = (event) => {
     console.log(event);
-    let quiz = event.target.value
-    this.setState({selectedQuiz: quiz });
-  }
+    let quiz = event.target.value;
+    this.setState({ selectedQuiz: quiz });
+  };
 
-  render(){
-    return(
-    <input type="button" className='QuizSelection' key={ e.id }  value={e.title} />
-    )
+  render() {
+    return (
+      <div>
+        {/* <input
+          type="button"
+          className="QuizSelection"
+          key={e.id}
+          value={e.title}
+        /> */}
+      </div>
+    );
   }
 }
-export default PlayButton
+export default PlayButton;
