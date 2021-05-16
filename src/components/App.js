8a7e93767d95ec572c10f876b25f3fc8d9ba086a
Fei Ye <feiye@Feis-MacBook-Pro.local>
2021-05-16 11:00:10 +1000

    updated App.js for authentication

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import GamePlayPage from "./pages/GamePlayPage";
@@ -10,26 +10,65 @@ import QuizCreatePage from "./pages/QuizCreatePage";
 import QuizIndexPage from "./pages/QuizIndexPage";
 import QuizItem from "./QuizItem";
 import Nav from "./Nav";
+import axios from "axios";
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={HomePage} />;
-          <Route exact path="/gameplay" component={GamePlayPage} />;
-          <Route exact path="/gamestart" component={GameStartPage} />;
-          <Route exact path="/host-sign-in" component={HostSignInPage} />;
-          <Route exact path="/host-sign-up" component={HostSignUpPage} />;
-          <Route exact path="/player-sign-in" component={PlayerSignInPage} />;
-          <Route exact path="/quiz-create" component={QuizCreatePage} />;
-          <Route exact path="/quiz-index" component={QuizIndexPage} />;
-        </Switch>
-        <QuizItem />
-      </div>
-    </Router>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoggedIn: false,
+      user: {},
+    };
+    this._handleLogin = this._handleLogin.bind(this);
+    this._handleLogout = this._handleLogout.bind(this);
+  }
+  componentDidMount() {
+    this.loginStatus();
+  }
+  loginStatus = () => {
+    axios
+      .get("http://localhost:3001/logged_in", { withCredentials: true })
+      .then((response) => {
+        if (response.data.logged_in) {
+          this.handleLogin(response);
+        } else {
+          this.handleLogout();
+        }
+      })
+      .catch((error) => console.log("api errors:", error));
+  };
+  _handleLogin = (data) => {
+    this.setState({
+      isLoggedIn: true,
+      user: data.user,
+    });
+  };
+  _handleLogout = () => {
+    this.setState({
+      isLoggedIn: false,
+      user: {},
+    });
+  };
+  render() {
+    return (
+      <Router>
+        <div>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={HomePage} />;
+            <Route exact path="/gameplay" component={GamePlayPage} />;
+            <Route exact path="/gamestart" component={GameStartPage} />;
+            <Route exact path="/host-sign-in" component={HostSignInPage} />;
+            <Route exact path="/host-sign-up" component={HostSignUpPage} />;
+            <Route exact path="/player-sign-in" component={PlayerSignInPage} />;
+            <Route exact path="/quiz-create" component={QuizCreatePage} />;
+            <Route exact path="/quiz-index" component={QuizIndexPage} />;
+          </Switch>
+          <QuizItem />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
