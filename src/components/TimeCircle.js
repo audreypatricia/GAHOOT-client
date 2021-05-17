c90a83880c5f2940e85a7735f5b031e1cb463064
SamGelwyn <78458599+SamGelwyn@users.noreply.github.com>
2021-05-17 11:31:37 +1000

    TimeCircle

---
+++ src/components/TimeCircle.js
@@ -0,0 +1,26 @@
+import React from "react";
+export default function TimeCircle() {
+  const [timer, setTimer] = React.useState(30);
+  const id = React.useRef(null);
+  const clear = () => {
+    window.clearInterval(id.current);
+  };
+  React.useEffect(() => {
+    id.current = window.setInterval(() => {
+      setTimer((time) => time - 1);
+    }, 1000);
+    return () => clear();
+  }, []);
+
+  React.useEffect(() => {
+    if (timer === 0) {
+      clear();
+    }
+  }, [timer]);
+
+  return (
+    <div className="TimeCircle">
+      <div>Time left : {timer} </div>
+    </div>
+  );
+}
