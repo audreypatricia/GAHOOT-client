e07e5bce1243f0a9cd948d95a2063aacdb05cb3f
Audrey Patricia <audreypatricia@Audreys-MacBook-Pro.local>
2021-05-17 12:53:49 +1000

    pull

--- src/components/pages/GamePlayPage.js
+++ src/components/pages/GamePlayPage.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
+import DisplayQuestion from '../DisplayQuestion';
+import axios from 'axios';
 
 class GamePlayPage extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {
+          quiz_id: 19,
+          questions: [],
+        }
+
+    axios.get('https://gahoot-server.herokuapp.com/questions.json').then( (result) => {
+      console.log(result.data);
+      let data = result.data;
+      let questions = [];
+      for(let i = 0; i < data.length; i++) {
+        if(data[i].quiz_id === this.state.quiz_id){
+          questions.push(data[i]);
+        }
+      }
+      console.log(questions);
+    })
+
+
     }
-    render() { 
-        return ( <div><h1>Gameplay</h1></div> );
+
+
+    render() {
+      return (
+        <div>
+          <h1>Gameplay</h1>
+          <DisplayQuestion questions={this.state.questions} />
+        </div>
+      );
     }
 }
- 
+
 export default GamePlayPage;
