e89c469de23f98a5475ca4c6fdd8f9c46cdedb33
Audrey Patricia <audreypatricia@Audreys-MacBook-Pro.local>
2021-05-18 15:25:49 +1000

    join components

--- src/components/pages/GameStartPage.js
+++ src/components/pages/GameStartPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PinBox from '../PinBox/PinBox'
 import { Link } from 'react-router-dom';
+import GamePlayPage from './GamePlayPage';
 
 class GameStartPage extends Component {
     constructor(props) {
@@ -15,11 +16,14 @@ class GameStartPage extends Component {
           <div>
           <h1>Game start</h1>
           <PinBox quiz_id={ this.props.match.params.id } host={ this.state.host }/>
-          <StartGameButton quiz_id={this.props.match.params.id}/>
+
+          <GamePlayPage quiz_id={this.props.match.params.id}/>
           </div> );
     }
 }
 
+//if need a new page for game:
+// <StartGameButton quiz_id={this.props.match.params.id}/>
 class StartGameButton extends Component {
   constructor(props){
     super(props);
@@ -32,6 +36,7 @@ class StartGameButton extends Component {
     return (
       <div className="button">
     <Link to={"/gameplay/" + this.props.quiz_id }>Start Game</Link>
+
     </div>
       )
    }
