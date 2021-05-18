e9aebc665d00521d1288c10c9110a6ee5e9f68a3
Audrey Patricia <audreypatricia@Audreys-MacBook-Pro.local>
2021-05-18 16:33:20 +1000

    play game merged

--- src/components/pages/GamePlayPage.js
+++ src/components/pages/GamePlayPage.js
@@ -13,8 +13,11 @@ class GamePlayPage extends Component {
         // quiz_id: this.props.match.params.id,
         quiz_id: this.props.quiz_id,
         questions: [],
+        roundOver: false,
+
 
       }
+
 
     axios.get('https://gahoot-server.herokuapp.com/questions.json').then( (result) => {
       console.log(result.data);
@@ -30,6 +33,7 @@ class GamePlayPage extends Component {
       this.setState({
         questions: questions,
         activeQuestion: 0,
+
       });
       console.log(questions);
     })
@@ -38,36 +42,50 @@ class GamePlayPage extends Component {
   }
 
   checkAnswer = (answer) => {
-
+// to check answer
+  console.log(this.state.roundOver);
+  this.setState({roundOver: true});
     if (answer == this.state.questions[this.state.activeQuestion].answer_options[4]) {
       console.log('You were right!');
 
-      this.setState((prevState) => ({
-        activeQuestion: prevState.activeQuestion + 1
-      }));
+      // this.setState((prevState) => ({
+      //   activeQuestion: prevState.activeQuestion + 1
+      // }));
     } else {
       console.log('WRONG');
     }
+
   };
 
   renderGame = () => {
+    console.log(this.props.startGame);
+    if(this.props.startGame === false){
+      return <div></div>
+    }
 
     if (this.state.activeQuestion === this.state.questions.length) {
       return <h1>You win!!!</h1>;
     }
+
+    if( this.state.roundOver === true ){
+      return <div>Waiting for other players to answer</div>
+    }
+
+    console.log(this.state.roundOver); //true
     return (
       <React.Fragment>
         <DisplayQuestion question={this.state.questions[this.state.activeQuestion].question} />
         <OptionList
           answer_options={this.state.questions[this.state.activeQuestion].answer_options}
           checkAnswer={this.checkAnswer}
+          roundOver ={this.state.roundOver}
         />
       </React.Fragment>
     );
   };
 
   render() {
-    if(this.state.questions.length === 0) return <p>Loading</p>;
+    if(this.state.questions.length === 0) return <p>...</p>;
 
     return (
       <GameWrapper>{this.renderGame()}</GameWrapper>
--- src/components/pages/GameStartPage.js
+++ src/components/pages/GameStartPage.js
@@ -7,17 +7,26 @@ class GameStartPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-        host: this.props.user
+        host: this.props.user,
+        startGame: false,
       }
+      this.startGame = this.startGame.bind(this);
     }
+
+    startGame(){
+      this.setState({startGame: true});
+    }
+
     render() {
       console.log('GameStart host: ', this.state.host)
+      console.log(this.state.startGame);
         return (
           <div>
           <h1>Game start</h1>
           <PinBox quiz_id={ this.props.match.params.id } host={ this.state.host }/>
+          <button onClick={this.startGame}>Start the game!</button>
 
-          <GamePlayPage quiz_id={this.props.match.params.id}/>
+          <GamePlayPage quiz_id={this.props.match.params.id} startGame={this.state.startGame}/>
           </div> );
     }
 }
