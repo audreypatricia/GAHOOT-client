a94df75a3edec8b3a341a5178995fb1cb48e87bd
Audrey Patricia <audreypatricia@Audreys-MacBook-Pro.local>
2021-05-19 17:26:50 +1000

    change

--- src/components/pages/GamePlayPage.js
+++ src/components/pages/GamePlayPage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import DisplayQuestion from '../DisplayQuestion';
 import OptionList from '../OptionList';
 import TimeCircle from '../TimeCircle';
-import ScoreBoard from '../Scoreboard/Scoreboard';
+import ScoreBoard from '../Scoreboard/Scoreboard.js';
 import axios from 'axios';
 
 import styled from 'styled-components';
