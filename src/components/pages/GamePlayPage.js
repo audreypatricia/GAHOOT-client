1a5b1eb07480b1b014bdaf839c8b160ac3d68364
SamGelwyn <78458599+SamGelwyn@users.noreply.github.com>
2021-05-19 13:39:42 +1000

    working time

--- src/components/pages/GamePlayPage.js
+++ src/components/pages/GamePlayPage.js
@@ -46,9 +46,9 @@ class GamePlayPage extends Component {
     this.setState((prevState) => ({
       activeQuestion: prevState.activeQuestion + 1
     }));
-    this.setState((prevState) => ({
-      seconds: prevState.seconds = 0
-    }));
+    // this.setState((prevState) => ({
+    //   seconds: prevState.seconds = 0
+    // }));
   }
 
   checkAnswer = (answer) => {
