79a3994de37913439209840201b8063d84e02aa3
Fei Ye <feiye@Feis-MacBook-Pro.local>
2021-05-20 12:48:36 +1000

    modify css

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -14,17 +14,23 @@ class HomePage extends Component {
         <div className="containerHostSignUpPage">
           <h1>Welcome to Gahoot!</h1>
           <div className="grid">
-          <div className="HostPageForm">
-            <Link to="/host-sign-in"><div className="HomePageDiv">Host Sign In</div></Link>
-          </div>
+            <div className="HomePageForm">
+              <Link to="/host-sign-in">
+                <div className="HomePageDiv">Host Sign In</div>
+              </Link>
+            </div>
 
-          <div className="HostPageForm">
-            <Link to="/host-sign-up"><div className="HomePageDiv">Host Sign Up</div></Link>
-          </div>
+            <div className="HomePageForm">
+              <Link to="/host-sign-up">
+                <div className="HomePageDiv">Host Sign Up</div>
+              </Link>
+            </div>
 
-          <div className="HostPageForm">
-            <Link to="/player-sign-in"><div className="HomePageDiv">Player Sign In</div></Link>
-          </div>
+            <div className="HomePageForm">
+              <Link to="/player-sign-in">
+                <div className="HomePageDiv">Player Sign In</div>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
